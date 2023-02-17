eb12da544fb800c52e0183044da8d641461472e3
sun.hwang.kr@gmail.com <sun.hwang.kr@gmail.com>
2023-02-17 10:50:27 +0900

    Ch5.1.2 사용자 요청 처리

--- Servlet_JSP/Day5_2023.02.17.Fri/BoardWeb/src/main/java/com/ssamz/web/user/InsertUserServlet.java
+++ Servlet_JSP/Day5_2023.02.17.Fri/BoardWeb/src/main/java/com/ssamz/web/user/InsertUserServlet.java
@@ -10,15 +10,16 @@ import com.ssamz.biz.user.UserDAO;
 import com.ssamz.biz.user.UserVO;
 
 /**
- * Servlet implementation class InsertUserServlet
+ * Ch5.1.2 사용자 요청 처리
  */
 public class InsertUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		// 1. 사용자 입력 정보 추출
-    	// 인코딩 처리
+		// 인코딩 처리
 		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
@@ -33,24 +34,23 @@ public class InsertUserServlet extends HttpServlet {
 		System.out.println("이름 : " + name);
 		System.out.println("권한 : " + role);
 		System.out.println("자기 소개 : " + selfInfo);
-		for(String language : languages) {
+		for (String language : languages) {
 			System.out.println(language + ", ");
 		}
 		System.out.println("");
 		System.out.println("나이 : " + age);
-		
+
 		// 2. DB 연동 처리
-		UserVO vo= new UserVO();
+		UserVO vo = new UserVO();
 		vo.setId(id);
 		vo.setPassword(password);
 		vo.setName(name);
 		vo.setRole(role);
-		
-		
+
 		UserDAO dao = new UserDAO();
 		dao.insertUserVO(vo);
-		
+
 		// 3. 화면 이동
-		response.sendRedirect("login.html");		
+		response.sendRedirect("login.html");
 	}
 }
