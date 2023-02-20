e8ee3b04d2fc3dd256f1030c2bab8631f9d3747c
sun.hwang.kr@gmail.com <sun.hwang.kr@gmail.com>
2023-02-20 11:30:15 +0900

    7.2.2 글 등록 구현(insertBoard.html, InsertBoardServlet.java)

---
+++ Servlet_JSP/Day6_2023.02.20.Mon/BoardWeb/src/main/java/com/ssamz/web/board/InsertBoardServlet.java
@@ -0,0 +1,51 @@
+package com.ssamz.web.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ssamz.biz.board.BoardDAO;
+import com.ssamz.biz.board.BoardVO;
+import com.ssamz.biz.user.UserDAO;
+import com.ssamz.biz.user.UserVO;
+
+@WebServlet("/insertBoard.do")
+
+public class InsertBoardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String encoding;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 1. 사용자 입력 정보 추출
+		ServletContext context= getServletContext();
+		encoding=context.getInitParameter("boardEncoding");
+		request.setCharacterEncoding("UTF-8");
+		
+		String title = request.getParameter("title");
+		String writer = request.getParameter("writer");
+		String content = request.getParameter("content");
+
+		// 2. DB 연동 처리
+		BoardVO vo = new BoardVO();
+		vo.setTitle(title);
+		vo.setWriter(writer);
+		vo.setContent(content);
+
+
+		BoardDAO dao = new BoardDAO();
+		dao.insertBoard(vo);
+
+		// 3. 화면 이동
+		//response.sendRedirect("getBoardList.do");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("getBoardList.do");
+		dispatcher.forward(request, response);
+	}
+}
