53e8e375f0b3128f7775f23a4c3483993ae5dc1b
sun.hwang.kr@gmail.com <sun.hwang.kr@gmail.com>
2023-02-24 16:48:46 +0900

    13.2.2 글 목록 검색 구현
    13.2.2 글 목록 검색 구현

--- Servlet_JSP/Day10_2023.02.24.Fri/BoardWeb/src/main/java/com/ssamz/web/controller/DispatcherServlet.java
+++ Servlet_JSP/Day10_2023.02.24.Fri/BoardWeb/src/main/java/com/ssamz/web/controller/DispatcherServlet.java
@@ -1,12 +1,18 @@
 package com.ssamz.web.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ssamz.biz.board.BoardDAO;
+import com.ssamz.biz.board.BoardVO;
 
 @WebServlet("*.do")
 public class DispatcherServlet extends HttpServlet {
@@ -14,28 +20,57 @@ public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 1. 사용자 요청 path를 추출한다.
 		String uri = request.getRequestURI();
 		String path = uri.substring(uri.lastIndexOf("/"));
-		
+
 		// 2. 추출된 path 정보에 따라 요청을 분기 처리한다.
-		if(path.equals("/login.do")) {
-			System.out.println("로그인 처리");			
-		} else if(path.equals("/insertUser.do")) {
-			System.out.println("회원가입 처리");			
-		} else if(path.equals("/logout.do")) {
-			System.out.println("로그아웃 처리");			
-		} else if(path.equals("/insertBoard.do")) {
-			System.out.println("글 등록 처리");			
-		} else if(path.equals("/updateBoard.do")) {
-			System.out.println("글 수정 처리");			
-		} else if(path.equals("/deleteBoard.do")) {
-			System.out.println("글 삭제 처리");			
-		} else if(path.equals("/getBoard.do")) {
-			System.out.println("글 상세 조회 처리");			
-		} else if(path.equals("/getBoardList.do")) {
-			System.out.println("글 목록 검색 처리");			
-		} 
+		if (path.equals("/login.do")) {
+			System.out.println("로그인 처리");
+		} else if (path.equals("/insertUser.do")) {
+			System.out.println("회원가입 처리");
+		} else if (path.equals("/logout.do")) {
+			System.out.println("로그아웃 처리");
+		} else if (path.equals("/insertBoard.do")) {
+			System.out.println("글 등록 처리");
+		} else if (path.equals("/updateBoard.do")) {
+			System.out.println("글 수정 처리");
+		} else if (path.equals("/deleteBoard.do")) {
+			System.out.println("글 삭제 처리");
+		} else if (path.equals("/getBoard.do")) {
+			System.out.println("글 상세 조회 처리");
+		} else if (path.equals("/getBoardList.do")) {
+			System.out.println("글 목록 검색 처리");
+				
+			//1. 검색 입력 정보 추출
+			String searchCondition = request.getParameter("searchCondition");
+			String searchKeyword = request.getParameter("searchKeyword");
+
+			// Null Check
+			if (searchCondition == null)
+				searchCondition = "TITLE";
+			if (searchKeyword == null)
+				searchKeyword = "";
+
+			// 세션에 검색 관련 정보를 저장한다.
+			HttpSession session = request.getSession();
+			session.setAttribute("condition", searchCondition);
+			session.setAttribute("keyword", searchKeyword);
+
+			// 2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			vo.setSearchCondition(searchCondition);
+			vo.setSearchKeyword(searchKeyword);
+
+			BoardDAO boardDAO = new BoardDAO();
+			List<BoardVO> boardList = boardDAO.getBoardList(vo);
+			
+			// 3. 화면 이동
+			request.setAttribute("boardList", boardList);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/getBoardList.jsp");
+			dispatcher.forward(request, response); 
+		}
 	}
 }
