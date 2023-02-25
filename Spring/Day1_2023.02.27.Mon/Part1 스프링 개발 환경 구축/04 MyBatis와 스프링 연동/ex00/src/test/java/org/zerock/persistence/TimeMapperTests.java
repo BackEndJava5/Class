a012020d4ebd5f6d97a19098901754780380b156
sun.hwang.kr@gmail.com <sun.hwang.kr@gmail.com>
2023-02-25 20:19:46 +0900

    4.2.3 XML 매퍼와 같이 쓰기 ( getTime2() )

--- Spring/Day1_2023.02.27.Mon/Part1 스프링 개발 환경 구축/04 MyBatis와 스프링 연동/ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
+++ Spring/Day1_2023.02.27.Mon/Part1 스프링 개발 환경 구축/04 MyBatis와 스프링 연동/ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -25,4 +25,12 @@ public class TimeMapperTests {
 		log.info(timeMapper.getClass().getName());
 		log.info(timeMapper.getTime());
 	}
+	
+	@Test
+	public void tetGetTime2() {
+
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
+
+	}
 }
