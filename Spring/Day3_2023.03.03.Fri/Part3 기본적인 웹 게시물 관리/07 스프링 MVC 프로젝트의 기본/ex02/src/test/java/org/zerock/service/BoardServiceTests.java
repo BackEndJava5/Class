1ba43b07bb41058803fa093764b18b6e93cc0d22
Sun Hwang <pigglet@korea.ac.kr>
2023-03-03 10:29:44 +0900

    9.2.2 목록(리스트) 작업의 구현과 테스트

--- Spring/Day3_2023.03.03.Fri/Part3 기본적인 웹 게시물 관리/07 스프링 MVC 프로젝트의 기본/ex02/src/test/java/org/zerock/service/BoardServiceTests.java
+++ Spring/Day3_2023.03.03.Fri/Part3 기본적인 웹 게시물 관리/07 스프링 MVC 프로젝트의 기본/ex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -43,4 +43,10 @@ public class BoardServiceTests {
 		log.info("생성된 게시물의 번호: " + board.getBno());
 	}
 
+	@Test
+	public void testGetList() {
+
+		service.getList().forEach(board -> log.info(board));
+	}
+
 }
